7e9f3bf613f46290c67beefaed3daa35499bbd9a
renanleonel <renanleonelpro@gmail.com>
2024-02-21 07:51:21 -0300

    feat: projects section

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function formatDate(date: string) {
   let currentDate = new Date();
   let targetDate = new Date(date);
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#101010",
+      },
+      textColor: {
+        primary: "#101010",
+      },
+      animation: {
+        pulse: "pulse 1s linear infinite",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
