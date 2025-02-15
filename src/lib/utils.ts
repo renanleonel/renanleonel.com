3e9527d90d71236340f90fac5e832c31af2172b0
renanleonel <renanleonelpro@gmail.com>
2025-02-14 23:49:27 -0300

    fix: remove counter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,23 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-  let currentDate = new Date();
   let targetDate = new Date(date);
 
   let timezoneOffset = targetDate.getTimezoneOffset() * 60 * 1000;
   targetDate = new Date(targetDate.getTime() + timezoneOffset);
-
-  let timeDifference = currentDate.getTime() - targetDate.getTime();
-
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-  let formattedDate = "";
-
-  if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`;
-  } else {
-    formattedDate = "Today";
-  }
 
   let fullDate = targetDate.toLocaleString("en-us", {
     month: "long",
@@ -30,5 +17,5 @@ export function formatDate(date: string) {
     year: "numeric",
   });
 
-  return `${fullDate} (${formattedDate})`;
+  return `${fullDate}`;
 }
